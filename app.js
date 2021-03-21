732c92f7d6f288084f9d308725a7bc8ba21b9feb
Nemanja Popic <nemanja.popic@playstudios-il.com>
2021-03-21 22:00:55 +0100

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Yo ghost, nice job!'));
+app.get('/', (req, res) => res.send('Welcome to my website!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
